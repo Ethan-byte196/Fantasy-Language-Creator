9a40302214f2eda2106030538d27902d7bf770e8
Ethan-byte196 <85854289+Ethan-byte196@users.noreply.github.com>
2023-10-07 15:31:30 -0400

    Some changes

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,23 @@
 /**
  * 
  */
+ function addTextBox() {
+  // Get a reference to the container element
+  const container = document.getElementById("textbox-container");
+
+  // Create a new text input element
+  const textBox = document.createElement("input");
+
+  // Set attributes for the text input
+  textBox.type = "text"; // Specifies it's a text input
+  textBox.placeholder = "Type English Here!!"; // Placeholder text (optional)
+
+  // Append the text input to the container
+  container.appendChild(textBox);
+}
+
+ // Wait for the DOM to be fully loaded before executing JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+	
+	addTextBox();
+});
