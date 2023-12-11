71494c354068e98713b03e540bb85cc8bc7360c0
Ethan Fuller <etjamir14@gmail.com>
2023-12-11 16:21:36 -0500

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,9 +1,21 @@
+function isLetter(char){
+    return ( (char >= 'A' &&  char <= 'Z') ||
+             (char >= 'a' &&  char <= 'z') );
+}
+
 function translator(){
     var text = document.getElementById("English").value;
     var translated;
 
     for(var i = 0; i < text.length; i++)
     {
+        var c = text.charAt(i).toLowerCase();
+
+        if(isLetter(c) == false)
+        {
+            continue;
+        }
+        
 
     }
 
